50d71da43ce177c25db24ae0f16f63c7e398f1c6
wwwalexengling18-blip <www.alexengling18@gmail.com>
2026-02-21 20:20:37 +0100

    Delete app_tmp/install_wasim_git.cmd

--- app_tmp/install_wasim_git.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-cd /d %~dp0
-
-echo Installiere WASimCommander (GitHub Version)...
-py -3.12 -m pip install --upgrade pip
-py -3.12 -m pip install git+https://github.com/mpaperno/WASimCommander.git
-
-echo Fertig
-pause
