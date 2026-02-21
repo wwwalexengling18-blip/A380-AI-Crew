79030fe2e8ab7919c34f1845b4cea24432652d48
wwwalexengling18-blip <www.alexengling18@gmail.com>
2026-02-21 20:17:52 +0100

    Update state_machine.py

--- app_tmp/state_machine.py
+++ app_tmp/state_machine.py
@@ -1,15 +1,38 @@
+import time
+
 class StateMachine:
-    def __init__(self):
-        self.state = "COLD_DARK"
+    def __init__(self, logger):
+        self.log = logger
+        self.state = "BOOT"
+        self.state_since = time.time()
 
-    def update(self, sim):
+    def set_state(self, new_state):
+        self.log.info(f"[SM] {self.state} -> {new_state}")
+        self.state = new_state
+        self.state_since = time.time()
 
-        if self.state == "COLD_DARK":
-            if sim.get("ELECTRICAL MAIN BUS VOLTAGE") > 20:
-                self.state = "POWERED"
+    def tick(self, ctx):
+        """
+        ctx enthält z.B.
+        ctx["sim"], ctx["cold_dark"], ctx["fbw_ready"], ctx["on_ground"], ...
+        """
+        if self.state == "BOOT":
+            self.set_state("COLD_DARK_MONITOR")
 
-        elif self.state == "POWERED":
-            if sim.get("GENERAL ENG COMBUSTION:1"):
-                self.state = "ENGINE_START"
+        elif self.state == "COLD_DARK_MONITOR":
+            # Cold & Dark lesen ist erlaubt. Wir warten, bis wir "ready to act" sind.
+            if ctx.get("fbw_ready"):
+                self.set_state("PREP")
+            else:
+                # weiter beobachten, aber noch nicht aggressiv steuern
+                pass
 
-        return self.state
+        elif self.state == "PREP":
+            # hier später: echte SOP-Checklisten, Batterie, ext power, IRS, etc.
+            # erstmal nur placeholder
+            if time.time() - self.state_since > 2:
+                self.set_state("IDLE")
+
+        elif self.state == "IDLE":
+            # später: Gate-to-Gate Flow
+            pass
