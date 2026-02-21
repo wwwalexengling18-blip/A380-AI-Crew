62cc196c1835f9918e2ab3a9b36aaf5b9a83a699
wwwalexengling18-blip <www.alexengling18@gmail.com>
2026-02-21 17:44:48 +0100

    Add SimClient class for SimConnect integration

--- app_tmp/simconnect_client.py
+++ app_tmp/simconnect_client.py
@@ -1,1 +1,13 @@
 
+from SimConnect import SimConnect
+
+class SimClient:
+    def __init__(self):
+        self.sm = None
+
+    def connect(self):
+        try:
+            self.sm = SimConnect()
+            print("[SIM] Verbindung erfolgreich")
+        except Exception as e:
+            print(f"[SIM] Fehler: {e}")
