8b62c3a577e199de7f6dadb74795e0a293e25060
wwwalexengling18-blip <www.alexengling18@gmail.com>
2026-02-21 20:23:00 +0100

    Create update.cmd

---
+++ app_tmp/update.cmd
@@ -0,0 +1,10 @@
+@echo off
+setlocal
+title A380-AI-Crew - UPDATE
+cd /d "%~dp0"
+
+echo Dieses Update-Skript ist fuer lokale Klone gedacht.
+echo Wenn du nur GitHub Web nutzt, brauchst du das nicht.
+echo.
+pause
+endlocal
