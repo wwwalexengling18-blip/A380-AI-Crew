3a906d1d0bc3f44aeb03346d9ff23fea8b93e937
wwwalexengling18-blip <www.alexengling18@gmail.com>
2026-02-21 20:15:40 +0100

    Update logger.py

--- app_tmp/logger.py
+++ app_tmp/logger.py
@@ -1,12 +1,34 @@
-import logging
 import os
+import time
+from datetime import datetime
 
-def setup_logger():
-    log_dir = "logs"
-    os.makedirs(log_dir, exist_ok=True)
+class Logger:
+    def __init__(self, log_dir=None, prefix="A380_AI"):
+        self.log_dir = log_dir
+        self.prefix = prefix
+        self._file = None
 
-    logging.basicConfig(
-        filename=os.path.join(log_dir, "a380_ai.log"),
-        level=logging.INFO,
-        format="%(asctime)s [%(levelname)s] %(message)s"
-    )
+        if self.log_dir:
+            os.makedirs(self.log_dir, exist_ok=True)
+            ts = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+            path = os.path.join(self.log_dir, f"{self.prefix}_{ts}.log")
+            self._file = open(path, "a", encoding="utf-8")
+
+        self.info("Logger init")
+
+    def _write(self, level, msg):
+        line = f"[{level}] {time.strftime('%H:%M:%S')} {msg}"
+        print(line)
+        if self._file:
+            self._file.write(line + "\n")
+            self._file.flush()
+
+    def info(self, msg): self._write("INFO", msg)
+    def warn(self, msg): self._write("WARN", msg)
+    def error(self, msg): self._write("ERROR", msg)
+
+    def close(self):
+        if self._file:
+            self.info("Logger close")
+            self._file.close()
+            self._file = None
